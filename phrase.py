2d614222bb17048df7e7713546b1de97f49ffdc4
Megg Gawat <megawatt@live.com>
2017-07-09 17:15:53 -0400

    Created more methods for class Phrase
    Added:
    - Count words
    - Calculate time
    - Calculate WPM
    - Get if attempt passed

--- phrase.py
+++ phrase.py
@@ -1,10 +1,36 @@
+import time
+
 class Phrase:
     def __init__(self, phrase):
         self.phrase = phrase
 
-    def get_attempt(text):
-        return input(text)
+    def count_words(self):
+        words = self.phrase.split(" ")
+        self.number_of_words = len(words)
 
+    def start_attempt(self, attempt = ""):
+        if attempt == "":
+            self.start_time()
+            self.attempt = input()
+            self.stop_time()
+        else:
+            self.attempt = attempt
 
-#test = Phrase("hello")
-#test.attempt()
+    def start_time(self):
+        self.start_time = time.time()
+
+    def stop_time(self):
+        self.total_time = time.time() - self.start_time
+
+    def calculate_wpm(self):
+        self.count_words()
+        self.wpm = round(self.number_of_words * (60/self.total_time), 0)
+
+    def get_passed(self):
+        if self.attempt == self.phrase:
+            print("You got it right!")
+            return True
+        else:
+            print("You got it wrong...")
+            return False
+
--- phrasetest.py
+++ phrasetest.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+import time
 import unittest
 from phrase import Phrase
 
@@ -10,8 +10,44 @@ class Test_TypingGame(unittest.TestCase):
         self.assertIsNotNone(self.phrase)
         self.assertEquals(self.phrase.phrase, sentence1)
 
-    # Get input will return 'This is a test sentence' during this test
-    def test_attempt_1(self):
+    def test_attempt(self):
         phrase = Phrase("This is a test sentence")
-        phrase.getattempt()
+        phrase.attempt("This is a test sentence")
+
+    def test_time(self):
+        phrase = Phrase("This is a test sentence")
+        now = time.time()
+        phrase.start_time()
+        self.assertAlmostEqual(phrase.start_time, now, 20)
+        total_time = time.time() - now
+        phrase.stop_time()
+        self.assertAlmostEqual(phrase.total_time, total_time, 20)
+
+    def test_count_number_of_words(self):
+        phrase1 = Phrase("This sentence has 5 words")
+        phrase1.count_words()
+        self.assertEquals(phrase1.number_of_words, 5)
+        phrase2 = Phrase("This is a really long sentence that has a bit more words than 5 words, "
+                         "in fact this sentence has around 30 words.")
+        phrase2.count_words()
+        self.assertEquals(phrase2.number_of_words, 23)
+
+    def test_calculate_wpm(self):
+        phrase = Phrase("This is yet another test sentence with just 10 words")
+        phrase.total_time = 60
+        phrase.calculate_wpm()
+        self.assertEquals(phrase.wpm, 10)
+        phrase2 = Phrase("This is yet another test sentence with just 10 words")
+        phrase2.total_time = 120
+        phrase2.calculate_wpm()
+        self.assertEquals(phrase2.wpm, 5)
+        phrase3 = Phrase("This is yet another test sentence with just 10 words")
+        phrase3.total_time = 10
+        phrase3.calculate_wpm()
+        self.assertEquals(phrase3.wpm, 60)
+
+    def test_passed(self):
+        phrase = Phrase("This is a test sentence")
+        phrase.start_attempt("This is a test sentence")
+        phrase.get_passed()
 
